9756d95942b153741550bbfd2c3a98df2ab099a7
Saulo <saulofernandezibar@gmail.com>
2021-03-04 15:15:09 -0300

    first commit

---
+++ aplicaciones/principal/views.py
@@ -0,0 +1,53 @@
+from django.shortcuts import render, redirect
+from .models import Persona
+from .forms import PersonaForm
+from django.views.decorators.csrf import csrf_exempt
+# Create your views here.
+
+def inicio(request):
+    personas = Persona.objects.all()
+    contexto = {
+        'personas':personas
+    }
+    return render(request,'index.html',contexto)
+
+
+@csrf_exempt
+def crearPersona(request):
+    if request.method == 'GET':
+        form = PersonaForm()
+        contexto = {
+            'form': form
+            }
+    else:
+        form = PersonaForm(request.POST)
+        contexto = {
+            'form': form
+            }
+        if form.is_valid():
+            form.save()
+            return redirect('index')
+    return render(request,'crear_persona.html',contexto)
+
+
+def editarPersona(request,id):
+    persona = Persona.objects.get(id = id)
+    if request.method == 'GET':
+        form = PersonaForm(instance = persona)
+        contexto = {
+            'form': form
+        }
+    else:
+        form = PersonaForm(request.POST, instance = persona)
+        contexto = {
+            'form': form
+        }
+        if form.is_valid():
+            form.save()
+            return redirect('index')
+    return render(request, 'crear_persona.html', contexto)
+
+def eliminarPersona(request,id):
+    persona = Persona.objects.get(id = id)
+    persona.delete()
+    return redirect('index')
